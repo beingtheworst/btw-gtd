c373399d3765b63e38806c66e5420d8e8a5ff877
Rinat Abdullin <rinat.abdullin@gmail.com>
2013-01-08 20:46:30 +0600

    starting to add scaffolding

---
+++ Source/Gtd.PublishedLanguage/ValueObjects.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Runtime.Serialization;
+
+namespace Gtd
+{
+    [Serializable]
+    public sealed class TenantId : AbstractIdentity<long>
+    {
+        public const string TagValue = "tenant";
+
+        public TenantId(long id)
+        {
+            Contract.Requires(id > 0);
+            Id = id;
+        }
+
+        public override string GetTag()
+        {
+            return TagValue;
+        }
+
+
+        [DataMember(Order = 1)]
+        public override long Id { get; protected set; }
+
+        public TenantId() { }
+    }
+
+
+}
