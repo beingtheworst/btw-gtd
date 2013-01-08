d818202af7c95a7fe4469c5c487afc3e31b36e65
Rinat Abdullin <rinat.abdullin@gmail.com>
2013-01-08 21:13:17 +0600

    starting to work on the domain

---
+++ Source/Gtd.PublishedLanguage/Message.cs
@@ -0,0 +1,10 @@
+namespace Gtd
+{
+    public abstract class Message
+    {
+        
+    }
+
+    public abstract class Command : Message {}
+    public abstract class Event : Message {}
+}
--- Source/Gtd.PublishedLanguage/ValueObjects.cs
+++ Source/Gtd.PublishedLanguage/ValueObjects.cs
@@ -27,5 +27,5 @@ namespace Gtd
         public TenantId() { }
     }
 
-
+    
 }
