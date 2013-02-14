ca3fa0eec0d8f24e74fc54361efff144b0c66644
Rinat Abdullin <rinat.abdullin@gmail.com>
2013-02-14 21:28:54 +0600

    Move Assign Due date command to separate file

--- Source/Gtd.Console/Commands/ExitCommand.cs
+++ Source/Gtd.Console/Commands/ExitCommand.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 
 namespace Gtd.Shell.Commands
 {
@@ -13,39 +12,4 @@ namespace Gtd.Shell.Commands
             Environment.Exit(0);
         }
     }
-
-    class AssignDueDateCommand : IConsoleCommand
-    {
-        public string[] Key { get { return new[] { "due", "dd" }; } }
-        public string Usage
-        {
-            get { return @"due <actionId> <date>
-Set optional Date when project or action is past due"; }
-        }
-        public void Execute(ConsoleEnvironment env, string[] args)
-        {
-            if (args.Length == 0)
-            {
-                throw new KnownConsoleInputError("Must provide action id and due date (or '-' to remove)");
-            }
-
-            var match = env.Session.MatchAction(args[0]);
-            DateTime span;
-            string value = string.Join(" ", args.Skip(1));
-            if (value.Trim() == "-")
-            {
-                env.Log.Trace("Removing due date");
-                env.TrustedSystem.When(new ProvideDueDateForAction(env.Session.SystemId, match.Id, DateTime.MinValue));
-                return;
-            }
-
-            if (!Parser.TryParseDate(value, out span))
-            {
-                throw new KnownConsoleInputError("Failed to parse due date '{0}'", value);
-            }
-            env.Log.Trace("Setting due date to {0}", span);
-            env.TrustedSystem.When(new ProvideDueDateForAction(env.Session.SystemId, match.Id, span));
-        }
-    }
-
 }
