4f5e86821a1cb13ce8cf92c6a158fb2efc1e7787
Rinat Abdullin <rinat.abdullin@gmail.com>
2013-02-14 21:28:21 +0600

    Ability to remove due and start dates by typing in dash

--- Source/Gtd.Console/Commands/ExitCommand.cs
+++ Source/Gtd.Console/Commands/ExitCommand.cs
@@ -14,36 +14,6 @@ namespace Gtd.Shell.Commands
         }
     }
 
-    class AssignStartDateCommand : IConsoleCommand
-    {
-        public string[] Key { get { return new[] {"start", "sd"}; } }
-        public string Usage
-        {
-            get
-            {
-                return @"due <actionId> <date>
-Set optional Date when project or action becomes available or relevant";
-            }
-        }
-        public void Execute(ConsoleEnvironment env, string[] args)
-        {
-            if (args.Length == 0)
-            {
-                throw new KnownConsoleInputError("Must provide action id and start date");
-            }
-
-            var match = env.Session.MatchAction(args[0]);
-            DateTime span;
-            string value = string.Join(" ", args.Skip(1));
-            if (!Parser.TryParseDate(value, out span))
-            {
-                throw new KnownConsoleInputError("Failed to parse start date '{0}'", value);
-            }
-            env.Log.Trace("Setting start date to {0}", span);
-            env.TrustedSystem.When(new ProvideStartDateForAction(env.Session.SystemId, match.Id, span));
-        }
-    }
-
     class AssignDueDateCommand : IConsoleCommand
     {
         public string[] Key { get { return new[] { "due", "dd" }; } }
@@ -56,12 +26,19 @@ Set optional Date when project or action is past due"; }
         {
             if (args.Length == 0)
             {
-                throw new KnownConsoleInputError("Must provide action id and due date");
+                throw new KnownConsoleInputError("Must provide action id and due date (or '-' to remove)");
             }
 
             var match = env.Session.MatchAction(args[0]);
             DateTime span;
             string value = string.Join(" ", args.Skip(1));
+            if (value.Trim() == "-")
+            {
+                env.Log.Trace("Removing due date");
+                env.TrustedSystem.When(new ProvideDueDateForAction(env.Session.SystemId, match.Id, DateTime.MinValue));
+                return;
+            }
+
             if (!Parser.TryParseDate(value, out span))
             {
                 throw new KnownConsoleInputError("Failed to parse due date '{0}'", value);
