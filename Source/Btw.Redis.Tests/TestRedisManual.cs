85d90cfc682537be775df075a615dcdcd9f8c1f0
Rinat Abdullin <rinat.abdullin@gmail.com>
2013-02-19 10:04:45 +0600

    Add missing files

---
+++ Source/Btw.Redis.Tests/TestRedisManual.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Gtd.Redis;
+using NUnit.Framework;
+using ServiceStack.Redis;
+
+namespace Btw.Redis.Tests
+{
+    [TestFixture, Explicit]
+    public sealed class TestRedisManual
+    {
+        [Test]
+        public void Name()
+        {
+            using (var test = new RedisClient())
+            using (var store = new RedisAppendOnlyStore(test))
+            {
+                store.Append("test", Encoding.UTF8.GetBytes("me data"), -1);
+            }
+            
+        } 
+
+        [Test]
+        public void ReadAll()
+        {
+            using (var test = new RedisClient())
+            using (var store = new RedisAppendOnlyStore(test))
+            {
+                var dataWithVersions = store.ReadRecords("test", 0, int.MaxValue).ToArray();
+                Console.WriteLine(dataWithVersions.Length);
+            }
+        }
+    }
+}
---
+++ Source/Btw.Redis/RedisAppendOnlyStore.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Gtd.Shell;
+using ServiceStack.Redis;
+
+namespace Gtd.Redis
+{
+    public class RedisAppendOnlyStore : IAppendOnlyStore
+    {
+        private readonly IRedisNativeClient _client;
+
+        public RedisAppendOnlyStore(IRedisNativeClient client)
+        {
+            _client = client;
+        }
+
+        public void Dispose()
+        {
+            _client.Dispose();
+        }
+
+        public void Append(string streamName, byte[] data, long expectedStreamVersion = -1)
+        {
+            if (data == null)
+                throw new ArgumentNullException("data");
+
+            if (data.Length == 0)
+                throw new ArgumentException("Buffer must contain at least one byte.");
+
+            _client.EvalInt(@"
+local expected = tonumber(ARGV[2])
+
+local storeVersion = redis.call('HLEN', 'STORE')
+local streamVersion = redis.call('LLEN', KEYS[1])
+
+if expected == -1 then
+    expected = streamVersion
+end
+
+if streamVersion == expected then
+
+    if streamVersion == 0 then
+        redis.call('RPUSH','STREAMS', KEYS[1])
+    end
+
+    redis.call('HSET', 'STORE',storeVersion+1,ARGV[1])
+    redis.call('RPUSH',KEYS[1],storeVersion+1)
+    return streamVersion+1
+else
+    return redis.error_reply('Stream version invalid. Expected ' .. expected .. ' actual ' .. streamVersion)
+end
+", 1, Encoding.UTF8.GetBytes(streamName), data, Encoding.UTF8.GetBytes(expectedStreamVersion.ToString()));
+            // TODO: catch stream conflicts
+
+         
+        }
+
+        public IEnumerable<StreamData> ReadRecords(string streamName, long afterVersion, int maxCount)
+        {
+
+            if (maxCount < 0)
+                throw new ArgumentOutOfRangeException("maxCount", "Must be zero or greater.");
+            if (maxCount == 0)
+                yield break;
+
+            var start = afterVersion < 0 ? 0 : afterVersion;
+            var end = maxCount == Int32.MaxValue ? maxCount : maxCount + afterVersion - 1;
+
+            var items = _client.Eval(@"
+local streamName = KEYS[1]
+local start = ARGV[1]
+local finish = ARGV[2]
+
+local set = redis.call('LRANGE', KEYS[1], ARGV[1], ARGV[2])
+local events = {}
+for k,v in pairs(set) do    
+    events[k] = redis.call('HGET', 'STORE', v)
+end
+
+return events", 1, Encoding.UTF8.GetBytes(streamName), Encoding.UTF8.GetBytes(start.ToString()), Encoding.UTF8.GetBytes(end.ToString()));
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                yield return new StreamData(i+start,items[i]);
+            }
+
+        }
+
+        public IEnumerable<DataWithKey> ReadRecords(long afterVersion, int maxCount)
+        {
+            if (maxCount < 0)
+                throw new ArgumentOutOfRangeException("maxCount", "Must be zero or greater.");
+            if (maxCount == 0)
+                yield break;
+
+            var start = afterVersion < 0 ? 0 : afterVersion;
+            var end = maxCount == Int32.MaxValue ? maxCount : maxCount + afterVersion - 1;
+
+            var items = _client.Eval(@"
+local storeVersion = redis.call('HLEN', 'STORE')
+
+local start = ARGV[1]
+local end = ARGV[2]
+
+if start > store then
+  start = store
+end
+
+if end > store then
+  end = store
+end
+
+
+local events = {}
+
+for i = start, end do
+    events[i] = redis.call('HGET','STORE',i)    
+end
+return events", 1, Encoding.UTF8.GetBytes(start.ToString()), Encoding.UTF8.GetBytes(end.ToString()));
+            throw new Exception(items.ToArray().ToString());
+        }
+
+        public void Close()
+        {
+            
+        }
+
+
+        public void ResetStore()
+        {
+            _client.Del("STORE");
+        }
+
+        public long GetCurrentVersion()
+        {
+            return _client.HLen("STORE");
+        }
+    }
+}
