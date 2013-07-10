e6279908212b819a34a99a26a00dad935bc41218
Rinat Abdullin <rinat.abdullin@gmail.com>
2013-07-10 15:53:54 +0600

    Nav tree does not hide selection

--- Source/Gtd.Client/Views/Navigation/NavigationView.Designer.cs
+++ Source/Gtd.Client/Views/Navigation/NavigationView.Designer.cs
@@ -34,12 +34,13 @@
             // treeView1
             // 
             this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.HideSelection = false;
             this.treeView1.Location = new System.Drawing.Point(0, 0);
             this.treeView1.Name = "treeView1";
             this.treeView1.Size = new System.Drawing.Size(305, 307);
             this.treeView1.TabIndex = 0;
             // 
-            // NavigationControl
+            // NavigationView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
