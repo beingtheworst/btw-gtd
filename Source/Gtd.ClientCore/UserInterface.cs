fd0c8511f528632452f9a16ad48b7a16749ac227
Rinat Abdullin <rinat.abdullin@gmail.com>
2013-07-25 19:18:21 +0600

    cosmetics

--- Source/Gtd.ClientCore/UserInterface.cs
+++ Source/Gtd.ClientCore/UserInterface.cs
@@ -6,22 +6,16 @@ using Gtd.Client.Views.Project;
 
 namespace Gtd.Client
 {
-    public class UserInterface {
-
-        public IMainMenu Menu { get;  set; }
-        public ILogView Log { get;  set; }
-        public INavigateBackView BackView { get;  set; }
-
-        public IAddStuffToInboxWizard AddStuffToInboxWizard { get;  set; }
-
-        public IAddActionToProjectWizard AddActionToProjectWizard { get;  set; }
-
-        public IDefineProjectWizard DefineProjectWizard { get;  set; }
-
-        public IInboxView InboxView { get;  set; }
-
-        public INavigationView Navigation { get;  set; }
-
-        public IProjectView ProjectView { get;  set; }
+    public class UserInterface
+    {
+        public IMainMenu Menu { get; set; }
+        public ILogView Log { get; set; }
+        public INavigateBackView BackView { get; set; }
+        public IAddStuffToInboxWizard AddStuffToInboxWizard { get; set; }
+        public IAddActionToProjectWizard AddActionToProjectWizard { get; set; }
+        public IDefineProjectWizard DefineProjectWizard { get; set; }
+        public IInboxView InboxView { get; set; }
+        public INavigationView Navigation { get; set; }
+        public IProjectView ProjectView { get; set; }
     }
 }
