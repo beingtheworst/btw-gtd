6830daee438c5d0bc17ff82a7eea894dc797b2a5
Kerry Street <kerrycstreet@gmail.com>
2013-08-29 13:19:50 -0700

    IInboxService and some initial VM

---
+++ Source/Gtd.Client.Core/Services/Inbox/IInboxService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Gtd.Client.Core.Models;
+
+namespace Gtd.Client.Core.Services.Inbox
+{
+    public interface IInboxService
+    {
+        IList<ItemOfStuff> AllStuffInInbox();
+        ItemOfStuff Get(StuffId stuffId);
+        void AddStuffToInbox(ItemOfStuff stuffId);
+        void TrashStuff(ItemOfStuff stuffId);
+    }
+}
--- Source/Gtd.Client.Core/ViewModels/InboxViewModel.cs
+++ Source/Gtd.Client.Core/ViewModels/InboxViewModel.cs
@@ -1,10 +1,16 @@
 ﻿using System.Collections.Generic;
 using Cirrious.MvvmCross.ViewModels;
+using Gtd.Client.Core.Models;
 
 namespace Gtd.Client.Core.ViewModels
 {
     public class InboxViewModel : MvxViewModel
     {
-
+        private IList<ItemOfStuff> _stuffInInbox;
+        public IList<ItemOfStuff> StuffInInbox
+        {
+            get { return _stuffInInbox; }
+            set { _stuffInInbox = value; RaisePropertyChanged(() => StuffInInbox); }
+        }
     }
 }
