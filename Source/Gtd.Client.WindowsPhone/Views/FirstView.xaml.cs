dce0b0be7e0471004060534cdf100c32340d9f44
Kerry Street <kerrycstreet@gmail.com>
2014-02-12 12:30:20 -0800

    update GTD Mobile to ref Mvx 3.1.1-stable release

---
+++ Source/Gtd.Client.WindowsPhone/Views/FirstView.xaml.cs
@@ -0,0 +1,12 @@
+using Cirrious.MvvmCross.WindowsPhone.Views;
+
+namespace Gtd.Client.WindowsPhone.Views
+{
+    public partial class FirstView : MvxPhonePage
+    {
+        public FirstView()
+        {
+            InitializeComponent();
+        }
+    }
+}
